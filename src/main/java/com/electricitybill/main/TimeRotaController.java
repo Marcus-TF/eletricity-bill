41d3078fe999930d0c87c34772a748916910e2b4
Marcus Túlio <zMarcust.st@gmail.com>
2023-06-27 10:24:49 -0300

    Refactor: adjusting location of TEAM ROUTE validations

--- src/main/java/com/electricitybill/main/TimeRotaController.java
+++ src/main/java/com/electricitybill/main/TimeRotaController.java
@@ -40,9 +40,6 @@ public class TimeRotaController {
             case 2:
                 System.out.println("Informe o Id que deseja atualizar: ");
                 int idUpdate = ler.nextInt();
-                if (Objects.isNull(timeRotaService.findById(idUpdate))) {
-                    throw new NoResultException("Team route not found!");
-                }
                 timeRota.setFuncionarioId(1);
                 timeRota.setTarefaRotaId(1);
                 System.out.println("Updated: " + timeRotaService.update(idUpdate, timeRota));
@@ -50,23 +47,14 @@ public class TimeRotaController {
             case 3:
                 System.out.println("Informe o Id que deseja deletar: ");
                 int idDelete = ler.nextInt();
-                if (Objects.isNull(timeRotaService.findById(idDelete))) {
-                    throw new NoResultException("Team route not found!");
-                }
                 System.out.println("Deleted: " + timeRotaService.delete(idDelete));
                 break;
             case 4:
                 System.out.println("Informe o Id que deseja buscar: ");
                 int idSearch = ler.nextInt();
-                if (Objects.isNull(timeRotaService.findById(idSearch))) {
-                    throw new NoResultException("Individual search not found!");
-                }
                 System.out.println("Individual search: " + timeRotaService.findById(idSearch));
                 break;
             case 5:
-                if (timeRotaService.findAll().isEmpty()) {
-                    throw new NoResultException("Search not found!");
-                }
                 System.out.println("Search: " + timeRotaService.findAll());
                 break;
             default:
--- src/main/java/com/electricitybill/service/impl/TimeRotaServiceImpl.java
+++ src/main/java/com/electricitybill/service/impl/TimeRotaServiceImpl.java
@@ -3,10 +3,12 @@ package com.electricitybill.service.impl;
 import com.electricitybill.entity.TimeRotaEntity;
 import com.electricitybill.repository.TimeRotaRepository;
 import com.electricitybill.service.TimeRotaService;
+import jakarta.persistence.NoResultException;
 import lombok.AllArgsConstructor;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 public class TimeRotaServiceImpl implements TimeRotaService {
@@ -15,11 +17,17 @@ public class TimeRotaServiceImpl implements TimeRotaService {
 
     @Override
     public List<TimeRotaEntity> findAll() {
+        if (timeRotaRepository.findAll().isEmpty()) {
+            throw new NoResultException("Search not found!");
+        }
         return timeRotaRepository.findAll();
     }
 
     @Override
     public TimeRotaEntity findById(int id) {
+        if (Objects.isNull(timeRotaRepository.findById(id))) {
+            throw new NoResultException("Individual search not found!");
+        }
         return timeRotaRepository.findById(id);
     }
 
@@ -30,11 +38,17 @@ public class TimeRotaServiceImpl implements TimeRotaService {
 
     @Override
     public boolean update(int id, TimeRotaEntity entity) throws SQLException {
+        if (Objects.isNull(timeRotaRepository.findById(id))) {
+            throw new NoResultException("Team route not found!");
+        }
         return timeRotaRepository.update(id, entity);
     }
 
     @Override
     public boolean delete(int id) throws SQLException {
+        if (Objects.isNull(timeRotaRepository.findById(id))) {
+            throw new NoResultException("Team route not found!");
+        }
         return timeRotaRepository.delete(id);
     }
 }
